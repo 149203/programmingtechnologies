2f9df3c9f2ad2cf44b851068342eef1ad2fe9b40
149203 <149203@gmail.com>
2019-02-11 15:44:51 -0800

    timeline_of_programming_languages.json

--- src/scripts/wikipedia_table_columns.js
+++ src/scripts/wikipedia_table_columns.js
@@ -1,42 +1,59 @@
 const fs = require('fs')
+const _ = require('lodash')
+
+// This script gets 'technologies' from Wikipedia table columns that have been scraped and put into a txt file.
+
+// Setup — set these constants then run the script
 
 const page_title = 'Timeline of programming languages'
+const filename = 'timeline_of_programming_languages.json'
 const page_url =
     'https://en.wikipedia.org/wiki/Timeline_of_programming_languages'
-const js_object = { page_title, page_url, technologies: [] }
-const col_number = 2
+const column_number = 2
+const scraped_data = '../scraped_pages/timeline_of_programming_languages.txt'
+
+// End setup
+
+// RegExps
+
+const rows = /(?<=(\|\-)).+?(?=(\|\-))/gs
+const columns = /(?!(\|))(?!(\s)).+/g
+const wiki_link = /(?<=\[\[).*?\|.*?(?=\]\])/
+const beginning_brackets = /^\[\[/
+const ending_syntax = /\s?(\(|\-|\||#|]).+/
+const sharp = /\s+sharp$/i
+
+// End RegExps
+
+const json_output = { page_title, filename, page_url, technologies: [] }
 
 try {
-    let text = fs.readFileSync('pages/test.txt', 'utf8')
-    text.match(/(?<=(\|\-)).+?(?=(\|\-))/gs).forEach(row => {
-        // fs.appendFileSync('results/test_results.txt', row)
-        row.match(/(?!(\|))(?!(\s)).+/g).forEach((col, i) => {
-            if (i + 1 === col_number) {
-                console.log(col)
-                // if it's a working link, output to file
-                if (/(?<=\[\[).*?\|.*?(?=\]\])/.test(col)) {
-                    js_object.technologies.push(format_name(col))
-                    //fs.appendFileSync('results/test_results.txt', '[{}]')
+    let text = fs.readFileSync(scraped_data, 'utf8')
+    text.match(rows).forEach(row => {
+        row.match(columns).forEach((column, i) => {
+            // if we're on the selected column number
+            if (i + 1 === column_number) {
+                // if it's a valid wikipedia link
+                if (wiki_link.test(column)) {
+                    json_output.technologies.push(format_name(column))
                 }
             }
         })
-        // regex for col: /(?!(\|))(?!(\s)).+/g
-        // regex for the link: /(?<=\[\[).*?\|.*?(?=\]\])/
     })
+
+    json_output.technologies = _.sortBy(_.uniq(json_output.technologies))
+
     fs.writeFileSync(
-        'results/test_results.json',
-        JSON.stringify(js_object, null, 4)
+        `../../lists_by_page/${filename}`,
+        JSON.stringify(json_output, null, 4)
     )
 } catch (e) {
     console.log('Error:', e.stack)
 }
 
-function format_name(col) {
-    const removed_beginning_brackets = col.replace(/^\[\[/, '')
-    const removed_ending_syntax = removed_beginning_brackets.replace(
-        /\s?(\(|\-|\||#).+/,
-        ''
-    )
-    const replace_sharp = removed_ending_syntax.replace(/\s+sharp$/i, '#')
-    return replace_sharp
+function format_name(column) {
+    return column
+        .replace(beginning_brackets, '')
+        .replace(ending_syntax, '')
+        .replace(sharp, '#')
 }
