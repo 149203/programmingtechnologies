48179bf3911a57d352ef9e81e8e622942c26b648
149203 <149203@gmail.com>
2019-02-14 11:24:45 -0800

    github_topics.json

---
+++ src/scripts/github_topics.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const _ = require('lodash')
+
+// This script gets 'technologies' from Wikipedia categories that have been scraped and put into a txt file.
+
+// Setup — set these constants then run the script
+
+const filename = 'github_topics'
+const url = 'https://github.com/topics'
+
+// End setup
+
+// RegExps
+
+const txt_phrase = /(?<=link-gray-dark">).+(?=<\/p>)/gm
+
+// End RegExps
+
+const json_output = {
+    filename: `${filename}.json`,
+    url,
+    technologies: [],
+}
+
+try {
+    let text = fs.readFileSync(`../scraped_pages_other/${filename}.txt`, 'utf8')
+
+    text.match(txt_phrase).forEach(phrase => {
+        json_output.technologies.push(phrase)
+    })
+
+    json_output.technologies = _.sortBy(
+        _.uniq(json_output.technologies),
+        technology => _.toLower(technology)
+    )
+
+    fs.writeFileSync(
+        `../../lists_by_page/${filename}.json`,
+        JSON.stringify(json_output, null, 4)
+    )
+} catch (e) {
+    console.log('Error:', e.stack)
+}
