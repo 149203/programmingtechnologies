c378a3b32619d5a111bc6fec7693eaa2f660d1bd
149203 <149203@gmail.com>
2019-02-13 18:58:17 -0800

    tiobe_top_50_to_100_languages.json

---
+++ src/scripts/tiobe_top_50_to_100_languages.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const _ = require('lodash')
+
+// This script gets 'technologies' from Wikipedia categories that have been scraped and put into a txt file.
+
+// Setup — set these constants then run the script
+
+const filename = 'tiobe_top_50_to_100_languages'
+const url = 'https://www.tiobe.com/tiobe-index/'
+
+// End setup
+
+// RegExps
+
+// End RegExps
+
+const json_output = {
+    filename: `${filename}.json`,
+    url,
+    technologies: [],
+}
+
+try {
+    let text = fs.readFileSync(`../scraped_pages_other/${filename}.txt`, 'utf8')
+
+    const phrases = _.split(text, ', ')
+
+    phrases.forEach(phrase => {
+        console.log(phrase)
+        json_output.technologies.push(phrase)
+    })
+
+    json_output.technologies = _.sortBy(
+        _.uniq(json_output.technologies),
+        technology => _.toLower(technology)
+    )
+
+    fs.writeFileSync(
+        `../../lists_by_page/${filename}.json`,
+        JSON.stringify(json_output, null, 4)
+    )
+} catch (e) {
+    console.log('Error:', e.stack)
+}
