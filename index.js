5a7767e86c79e6af707ef43a6b1db83fdbfe6d3c
149203 <149203@gmail.com>
2019-02-16 13:19:08 -0800

    Instructions

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('Please read the README to understand what this repo is about.')
+console.log(
+    'Please read the README to understand what this repo is about. Run src/scripts/popularity.js to compute popularity from lists.'
+)
