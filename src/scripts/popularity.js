66e6f2ab5f0ec40192078f975ec78c7463b732fa
149203 <149203@gmail.com>
2019-02-16 13:17:44 -0800

    all_popular_technologies.json; TODO: add remaining tech at popularity 10

--- src/scripts/popularity.js
+++ src/scripts/popularity.js
@@ -3,43 +3,84 @@ const _ = require('lodash')
 
 // Setup — set these constants then run the script
 
-const filename = 'stack_overflow_2018_developers_survey'
-const url = 'https://insights.stackoverflow.com/survey/2018/'
+const folder = '../../lists_to_weight_popularity'
 
-const file_folder = '../../lists_from_stack_overflow_2018_developer_survey'
+const folder_files = [
+    { filename: 'javascript.json', popularity: 60 },
+    { filename: 'stack_overflow_2018_developers_survey.json', popularity: 50 },
+    { filename: 'github_topics.json', popularity: 40 },
+    { filename: 'tiobe_index_top_50.json', popularity: 40 },
+    { filename: 'list_of_javascript_libraries.json', popularity: 30 },
+    { filename: 'wikipedia_solution_stack.json', popularity: 30 },
+    { filename: 'best_css_framework_list_top.json', popularity: 30 },
+    { filename: 'tiobe_top_50_to_100_languages.json', popularity: 20 },
+    { filename: 'tiobe_index_languages_canonical.json', popularity: 20 },
+]
 
 // End setup
 
-// RegExps
+// Create one big array of objects with all duplicates
+// Filter out exact duplicates
+// Console.log a list of difference in casing
+// Manually remove the duplicates that are a lower popularity
 
-const txt_phrase = /(?<=\/\/\s).+/gm
-
-// End RegExps
-
-const json_output = {
-    filename: `${filename}.json`,
-    url,
-    technologies: [],
-}
+let json_output = []
 
 try {
-    fs.readdirSync(file_folder).forEach(file => {
-        let scraped_tech = JSON.parse(
-            fs.readFileSync(`${file_folder}/${file}`, 'utf8')
+    fs.readdirSync(folder).forEach(json_file => {
+        let parsed_file = JSON.parse(
+            fs.readFileSync(`${folder}/${json_file}`, 'utf8')
         )
-        scraped_tech.technologies.forEach(technology => {
-            json_output.technologies.push(technology)
+        parsed_file.technologies.forEach(technology => {
+            const tech_obj = {}
+            tech_obj.name = technology
+            folder_files.forEach(file => {
+                if (file.filename === json_file) {
+                    tech_obj.popularity = file.popularity
+                }
+            })
+
+            json_output.push(tech_obj)
         })
     })
 
-    json_output.technologies = _.sortBy(
-        _.uniq(json_output.technologies),
-        technology => _.toLower(technology)
+    const json_output_ordered = _.orderBy(
+        json_output,
+        ['popularity', json_output => _.toLower(json_output.name)],
+        ['desc', 'asc']
     )
 
+    const json_output_no_exact_dupes = _.uniqBy(
+        json_output_ordered,
+        tech => tech.name
+    )
+
+    const close_dupes = json_output_no_exact_dupes.filter((tech, i, arr) => {
+        let result = []
+        arr.filter(other_tech => {
+            if (
+                // if there is a capitalization difference in a name
+                _.toLower(tech.name) === _.toLower(other_tech.name) &&
+                tech.name !== other_tech.name
+            ) {
+                result.push(tech, other_tech)
+            }
+        })
+        return Array.isArray(result) && result.length
+    })
+
+    if (!Array.isArray(close_dupes) || close_dupes.length) {
+        // view capitalization differences in names and correct them in the JSON file
+        console.log(
+            'Correct the capitalization in these duplicates:',
+            close_dupes
+        )
+        throw new Error('YOU HAVE CLOSE DUPLICATES. SEE THE CONSOLE LOG.')
+    }
+
     fs.writeFileSync(
-        `../../lists_by_page/${filename}.json`,
-        JSON.stringify(json_output, null, 4)
+        `../../complete_lists/all_popular_technologies.json`,
+        JSON.stringify(json_output_no_exact_dupes, null, 4)
     )
 } catch (e) {
     console.log('Error:', e.stack)
