e37006a84b56d9f5e32b08703e9f4081b1093736
149203 <149203@gmail.com>
2019-03-31 10:24:39 -0700

    added ids

---
+++ src/scripts/add_ids.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const _ = require('lodash')
+
+try {
+    let original_array = JSON.parse(
+        fs.readFileSync(
+            `../../complete_lists/programming_technologies.json`,
+            'utf8'
+        )
+    )
+
+    const array_with_ids = _.map(original_array, (technology, i) => {
+        technology._id = i + 1
+        return technology
+    })
+
+    console.log(array_with_ids)
+
+    fs.writeFileSync(
+        `../../complete_lists/programming_technologies_with_ids.json`,
+        JSON.stringify(array_with_ids, null, 4)
+    )
+} catch (e) {
+    console.log('Error:', e.stack)
+}
